de46bd6c5b5c83849cbc7206168076b026eba0e4
Menny Even Danan <menny@evendanan.net>
2025-07-23 23:13:55 +0900

    fix gradle setup

--- app/src/main/java/com/anysoftkeyboard/janus/app/App.kt
+++ app/src/main/java/com/anysoftkeyboard/janus/app/App.kt
@@ -1,10 +1,10 @@
 package com.anysoftkeyboard.janus.app
 
-import com.anysoftkeyboard.janus.database.Database
+import com.anysoftkeyboard.janus.database.AppDatabase
 import com.anysoftkeyboard.janus.network.Network
 
 class App(
-    private val database: Database,
+    private val database: AppDatabase,
     private val network: Network,
 ) {
 }
