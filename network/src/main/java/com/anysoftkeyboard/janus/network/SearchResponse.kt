b84acdcc013bfe87bb497c71f4fb984152a7055d
Menny Even Danan <menny@evendanan.net>
2025-08-07 03:01:45 +0000

    feat(network): Implement MediaWiki API Service

---
+++ network/src/main/java/com/anysoftkeyboard/janus/network/SearchResponse.kt
@@ -0,0 +1,20 @@
+package com.anysoftkeyboard.janus.network
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SearchResponse(
+    @Json(name = "query") val query: Query
+)
+
+@JsonClass(generateAdapter = true)
+data class Query(
+    @Json(name = "search") val search: List<SearchResult>
+)
+
+@JsonClass(generateAdapter = true)
+data class SearchResult(
+    @Json(name = "title") val title: String,
+    @Json(name = "snippet") val snippet: String
+)
