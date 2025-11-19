fa95ae641d8caaffab23eb92dcf305058e135df6
Menny Even Danan <menny@evendanan.net>
2025-11-19 16:23:27 -0500

    Set status bar color to theme background and invert isAppearanceLightStatusBars logic

--- app/src/main/java/com/anysoftkeyboard/janus/app/ui/theme/JanusTheme.kt
+++ app/src/main/java/com/anysoftkeyboard/janus/app/ui/theme/JanusTheme.kt
@@ -54,8 +54,8 @@ fun JanusTheme(
   if (!view.isInEditMode) {
     SideEffect {
       val window = (view.context as Activity).window
-      window.statusBarColor = colorScheme.primary.toArgb()
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+      window.statusBarColor = colorScheme.background.toArgb()
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
     }
   }
 
