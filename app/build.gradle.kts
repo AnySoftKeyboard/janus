0f4272383bcdec14e97a6ee8036370b476536ec9
Polyglot <ask@evendanan.net>
2025-11-24 03:43:41 +0000

    Auto-increment version code to 3 and name to 0.0.7 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.anysoftkeyboard.janus"
     minSdk = 21
     targetSdk = 35
-    versionCode = 2
-    versionName = "0.0.6"
+    versionCode = 3
+    versionName = "0.0.7"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
