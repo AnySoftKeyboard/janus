c44d2117b8ab7afa44e84c5f20f87a1bb8053e18
Polyglot <ask@evendanan.net>
2026-09-06 16:30:44 +0000

    Auto-increment version code to 14 and name to 0.1.9 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "com.anysoftkeyboard.janus"
     minSdk = libs.versions.minSdk.get().toInt()
     targetSdk = libs.versions.targetSdk.get().toInt()
-    versionCode = 13
-    versionName = "0.1.8"
+    versionCode = 14
+    versionName = "0.1.9"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
